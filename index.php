858cc843d5d219c72488f9e5dd074cf5ac6ff7a7
Sheffia Afiezha <sheffiaafiezhaprabaratatengker@gmail.com>
2021-07-17 01:31:03 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias adipisci enim, similique id nesciunt doloribus minima voluptates, error accusantium eos incidunt voluptatem fugit voluptatibus nulla totam, ullam recusandae non excepturi.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
